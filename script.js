8199052a8a0720d57a77cf180c47bccf2e26b602
SoxCloud <sogcinwan@gmail.com>
2025-07-18 16:28:58 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  document.querySelector('.nav-links').classList.toggle('active');
+}
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
